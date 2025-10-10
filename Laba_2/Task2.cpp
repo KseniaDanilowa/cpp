6aea8810a8f3065cad7968a9a542dc1431e172a5
KseniaDanilowa <132713461+KseniaDanilowa@users.noreply.github.com>
2025-10-11 02:30:23 +0300

    Add files via upload

---
+++ Laba_2/Task2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    const int MAX = 256;          
+    char str[MAX];
+
+    cout << "Введите строку: ";
+    cin.getline(str, MAX);      
+
+    
+    char* p = str;
+    while (*p) ++p;               
+    int len = p - str;            // разность указателей = длина
+    cout << "Длина строки: " << len << '\n';
+
+    
+    char* left = str;
+    char* right = str + len - 1;
+    while (left < right) {
+        char tmp = *left;
+        *left = *right;
+        *right = tmp;
+        ++left;
+        --right;
+    }
+    cout << "Перевернутая строка: " << str << '\n';
+
+    
+    char ch;
+    cout << "Какой символ подсчитать? ";
+    cin >> ch;
+
+    int count = 0;
+    for (char* q = str; *q; ++q)
+        if (*q == ch) ++count;
+
+    cout << "Символ '" << ch << "' встречается " << count << " раз(а).\n";
+    return 0;
+}
